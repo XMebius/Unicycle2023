5ee4a7c7d623e855461683221f421d04f69ebe59
Yixuan Chen <mebiusccc@proton.me>
2023-05-25 18:19:55 +0800

    勉强30s原地平衡 roll方向时间长会倒 pitch不能消除小幅度振荡

--- code/balance.c
+++ code/balance.c
@@ -44,8 +44,8 @@ int PWM_R=0,PWM_Y=0,PWMA_accel,PWMB_accel,PWMC_accel;                       //PW
 // int Voltage;  
 
 //静置后  Pitch_Zero=1.35  不静置 Pitch_Zero=3.82
-float Roll_Zero=0.094849/*0.094850~0.094840*/, Roll_Zero1=5.63;      //Roll_Zero1：记录值，龙邱的代码中Roll_Zero=-3,3,往陀螺仪方向倾斜是负数，反之为正数
-float Pitch_Zero=-0.30,Pitch_Zero1=5.63;     //Pitch_Zero1:
+float Roll_Zero=0.2/*0.15~0.2*//*0.094850~0.094860*//*, Roll_Zero1=5.63*/;       //面前，远离陀螺仪位置，往前Roll变小，往后roll变大
+float Pitch_Zero=-0.255/*2.080*//*2.050~2.095*//*,Pitch_Zero1=5.63*/;     // 面前，远离陀螺仪放置，往左偏零点小，往右越大
 //float Roll_Zero=0.0038, Roll_Zero1=5.63;      //Roll_Zero1：记录值，龙邱的代码中Roll_Zero=-3,3,往陀螺仪方向倾斜是负数，反之为正数
 //float Pitch_Zero=-0.024,Pitch_Zero1=5.63;     //Pitch_Zero1:
 float Yaw_Zero=0.0;                     //XY轴角度零点，与机械有关，影响稳定性
@@ -55,11 +55,11 @@ float error_Camera, error_Blance;       //摄像头和电磁误差
 float error_of_CameraOrBalance;         //转向环误差
 
 /****************************************速度为160左右*************************************************************/
-float R_Balance_KP=-11000/*-7550/-12000/-5600,开始出现较大幅度振荡*/, R_Balance_KI=1   ,  R_Balance_KD=-2000/*-860/-520/-1500*/  ;          //AB电机左右倾角4500 15 275
+float R_Balance_KP=-15400/*-11000,-13400开始出现较大幅度振荡*/, R_Balance_KI=0/*-10*/   ,  R_Balance_KD=-4150/*-2250*/  ;          //AB电机左右倾角4500 15 275
 float Y_Balance_KP=-1.05  ,  Y_Balance_KI=00.0  ,  Y_Balance_KD=-25.0  ;      //AB电机旋转角度
 float R_Velocity_KP=00  ,  R_Velocity_KI=0   ;                               //AB电机速度环
-float P_Balance_KP=-760/*-850*/, P_Balance_KI=1,P_Balance_KD=-125;                //C电机前后倾角
-float P_Velocity_KP=-0.215,P_Velocity_KI=0/*-0.05*/;                                //C电机速度环
+float P_Balance_KP=-2350/*>2250 -1450 -2850出现大幅度振荡*/, P_Balance_KI=0,P_Balance_KD=-40/*<-180*//*300*/;                //C电机前后倾角
+float P_Velocity_KP=0/*-0.287*//*2.287*/,P_Velocity_KI=0/*-0.05*/;                                //C电机速度环
 
 ///****************************************速度为160左右*************************************************************/
 //float R_Balance_KP=-7600/*-8000/-3600,开始出现较大幅度振荡*/, R_Balance_KI=1   ,  R_Balance_KD=-760/*-820/420*/  ;          //AB电机左右倾角4500 15 275
@@ -70,7 +70,7 @@ float P_Velocity_KP=-0.215,P_Velocity_KI=0/*-0.05*/;                            
 
 float Yaw_control_1,Yaw_control_2 = 0;
 float Yaw_mark;
-int Move_distance=1 , Move_distance_MAX=10;
+int Move_distance=0 /*, Move_distance_MAX=10*/;
 
 float Pitch=0.0f,Roll=0.0f,Yaw=0.0f,Pitch_temp=0.0f,Roll_temp=0.0f,Yaw_temp=0.0f;    //经过卡尔曼滤波后的姿态信息
 float gyro_yaw=0.0f,gyro_pitch=0.0f,gyro_roll=0.0f,gyro_yaw_temp=0.0f,gyro_pitch_temp=0.0f,gyro_roll_temp=0.0f;
@@ -82,7 +82,8 @@ void Balance(void)
     Encoder_A = encoder_get_count(ENCODER_1_QUADDEC);   //左电机 母板上编码器1，小车前进为负值
     Encoder_B = encoder_get_count(ENCODER_2_QUADDEC);   //右电机 母板上编码器2，小车前进为正值
     Encoder_C = encoder_get_count(ENCODER_3_QUADDEC);   //行进电机 母板上编码器3，小车前进为正值
-    printf("%hd,%hd,%hd,",Encoder_A,Encoder_B,Encoder_C);  //-3500的PWM对应的encoder的值为-3330
+//    printf("%hd,%hd,%hd,",Encoder_A,Encoder_B,Encoder_C);  //-3500的PWM对应的encoder的值为-3330
+    printf("%f,",Encoder_C);
     Encoder_C_Sum += Encoder_C;                     //行进电机编码器累加
 
     getKalmanPosition(&Pitch_temp,&Roll_temp,&Yaw_temp,&gyro_yaw_temp,&gyro_pitch_temp,&gyro_roll_temp);//获取姿态信息
@@ -90,7 +91,9 @@ void Balance(void)
     Roll=Roll_temp*40;
     gyro_pitch=gyro_pitch_temp*20;
     gyro_roll=gyro_roll_temp*40;
-
+//    printf("%f,%f",Pitch,gyro_pitch);
+    float temp=Pitch-Pitch_Zero;
+    printf("%f,%f,",temp,Roll);
 /************************保持直立以及转向运算*********************************************************************************************************************************/
 #ifdef cascade_pid   //串级pid
     PWM_R = P_Cascade_Pid_Ctrl(Roll_Zero);
@@ -98,11 +101,11 @@ void Balance(void)
     PWM_R = R_balance_Control(Roll, &Roll_Zero, gyro_roll);
 #endif
 
-//    if(++Flag_I==30)
-//    {
-//        PWM_Y = Y_balance_Control(/*error_of_CameraOrBalance*/0, 0,gyro_yaw);     //A B电机控制Z轴转动
-//        Flag_I=0;
-//    }
+    if(++Flag_I==30)
+    {
+        PWM_Y = Y_balance_Control(/*error_of_CameraOrBalance*/0, 0,gyro_yaw);     //A B电机控制Z轴转动
+        Flag_I=0;
+    }
     Motor_A = (short)+PWM_R ;//+ PWM_Y /*+ PWMA_accel*/;                                //最终控制量   PWM_Y?
     Motor_B = (short)-PWM_R ;//+ PWM_Y /*+ PWMA_accel*/;                                //最终控制量
 #ifdef cascade_pid
@@ -187,7 +190,7 @@ float P_balance_Control(float Angle,float Angle_Zero,float Gyro)
     Bias=Angle-Angle_Zero;                                               //获取偏差
     error+=Bias;                                                         //偏差累积
     error = constrain_float(error, -30, 30);                            //积分限幅
-    PWM=P_Balance_KP*Bias + P_Balance_KI*error + Gyro*P_Balance_KD/10;   //获取最终数值
+    PWM=P_Balance_KP*Bias + P_Balance_KI*error + Gyro*P_Balance_KD;   //获取最终数值
 //    printf("%f,%f,",P_Balance_KP,Bias);
     if(Start_Flag==0) PWM=0,error=0;                                     //停止时参数清零
 //    printf("%f,%f\n",PWM,Start_Flag);
@@ -262,6 +265,7 @@ float Velocity_Control_C(int encoder)
     Encoder_Integral += Encoder - Move_distance;                             //积分出位移
     Encoder_Integral = constrain_float(Encoder_Integral, -500, 500);        //积分限幅
     Velocity = Encoder * P_Velocity_KP + Encoder_Integral * P_Velocity_KI;   //获取最终数值
+
     if(Start_Flag==0) Encoder_Integral=0,Encoder=0,Velocity=0;               //停止时参数清零
     return Velocity;
 }
